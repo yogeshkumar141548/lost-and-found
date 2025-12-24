46ddc843f08b38a137a6ed895dc9cf33940643be
yogeshkumar141548 <yogesh.kumar141548@gmail.com>
2025-12-24 09:47:14 +0530

    Implement item loading and display functionality
    Fetches items from the server and displays them in the DOM.

---
+++ view-items.js
@@ -0,0 +1,26 @@
+async function loadItems() {
+    const response = await fetch("http://localhost:3000/items");
+    const items = await response.json();
+
+    const container = document.getElementById("items");
+    container.innerHTML = "";
+
+    items.forEach(item => {
+        const div = document.createElement("div");
+        div.style.border = "1px solid black";
+        div.style.margin = "10px";
+        div.style.padding = "10px";
+
+        div.innerHTML = `
+            <strong>${item.itemName}</strong><br>
+            Type: ${item.type}<br>
+            Description: ${item.description}<br>
+            Posted By: ${item.userId.name}<br>
+            Date: ${new Date(item.date).toLocaleDateString()}
+        `;
+
+        container.appendChild(div);
+    });
+}
+
+loadItems();
